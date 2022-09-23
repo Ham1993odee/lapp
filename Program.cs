b28a392cfaa77fc7a7a051a88ba8827bc8c380b4
Mohammad <mohammad.abdullah@iths.se>
2022-09-23 16:39:55 +0200

    my first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿HtmlGenerator myWebsite = new HtmlGenerator();
+myWebsite.hTml();
+myWebsite.h1();
+myWebsite.main();
+myWebsite.main2();
+
+
+class HtmlGenerator
+{
+    public void hTml()
+    {
+        Console.WriteLine("<!DOCTYPE html>" + 
+                          "\n<html>" +
+                          "\n<body>");
+    }
+
+    public void h1()
+    {
+        Console.WriteLine("<h1> Välkomna </h1>");
+    }
+
+    public void main()
+    {
+        Console.WriteLine("<main>" +
+                          "\n<p>Kurs om C#</p>" +
+                          "\n<p>Kurs om Databaser</p>");
+    }
+    public void main2()
+    {
+        Console.WriteLine("</main>" +
+                          "\n</body>" +
+                          "\n</html");
+
+    }
+}
